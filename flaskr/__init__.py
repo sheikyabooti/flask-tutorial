ac1e3f438fd66a40ac0c246167adb8071016661c
Chris Lee <christophermlee2000@yahoo.com>
2022-01-05 16:47:06 -0800

    blog functional
    auth.py
      login and register
    
    blog.py
      index.html as well as create, update, and delete

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,9 @@ import os
 
 from flask import Flask
 
+from flask import (
+    render_template
+)
 
 def create_app(test_config=None):
     # create and configure the app
@@ -27,10 +30,12 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    from . import auth
+    app.register_blueprint(auth.bp)
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    
     return app
     
